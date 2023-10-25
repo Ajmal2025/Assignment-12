f8aa829f4c18a1826105393c83d60a0e5f165b98
Ajmal2025 <139298518+Ajmal2025@users.noreply.github.com>
2023-10-25 21:54:36 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,13 @@
+
+
+//12.	Greetings: Start with the array you used in Exercise 11, 
+//but instead of just printing each person’s name, print a message to them.
+// The text of each message should be the same, 
+//but each message should be personalized with the person’s name.
+
+let names :string[]= [ 'Ashraf','Adeel','Rafiq'];
+
+for (let name of names)
+{
+
+console.log( `Assalam o Alaikum ${name}, have a great weekend`);}
